1e58d0f42ff72aec2a970f013f05398be2285713
Stuti Chourasia <stuti.chourasia@thomsonreuters.com>
2018-08-28 12:07:08 +0530

    cucumber file updated2

--- src/testcode/testrunner/Regression.java
+++ src/testcode/testrunner/Regression.java
@@ -3,15 +3,10 @@ package testcode.testrunner;
 import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
 import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
 import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-@RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(
-        jsonReport = "target/cucumber.json",
-        detailedReport = true,
-        detailedAggregatedReport = true,
-        toPDF = true,
-        outputFolder = "target/CucumberReports/ExtendedReport")
+@RunWith(Cucumber.class)
 @CucumberOptions(
         features = { "src/test/resources/features/" },
         glue = "", plugin = {
